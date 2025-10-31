e0ca8e86e8efdb86d1eeeb4fe7c0d65925ed75f2
UjjwalKarkeyy <ujjwalkarki.official@gmail.com>
2025-10-31 14:01:20 +0545

    changed base link

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,5 +2,5 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
-  base: '/Host_Crop_Disease_Pred_Frontend/',  // added this line
+  base: './',  // added this line
 })
